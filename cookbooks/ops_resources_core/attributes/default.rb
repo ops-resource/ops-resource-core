aa216d85d961308f91cc7a4c795e5e45ed6d1ee9
Petrik van der Velde <petrikvandervelde@gmail.com>
2015-03-24 16:08:50 +1300

    Adding the attribute for the consul base directory.

--- cookbooks/ops_resources_core/attributes/default.rb
+++ cookbooks/ops_resources_core/attributes/default.rb
@@ -2,6 +2,7 @@ meta_base_path = 'c:\\meta'
 default['paths']['meta'] = meta_base_path
 
 consul_base_path = 'c:\\ops\\consul'
+default['paths']['consul_base'] = consul_base_path
 default['paths']['consul_bin'] = "#{consul_base_path}\\bin"
 default['paths']['consul_data'] = "#{consul_base_path}\\data"
 default['paths']['consul_checks'] = "#{consul_base_path}\\checks"
