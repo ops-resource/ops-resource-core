c7429351474e8c2f3082ad9677eca01a657ba64d
Petrik van der Velde <petrikvandervelde@gmail.com>
2015-04-22 12:05:06 +1200

    Adding the Get-EnvironmentForLocalNode function and setting all webrequest calls to use basic parsing

--- scripts/utils/windows/Consul.ps1
+++ scripts/utils/windows/Consul.ps1
@@ -20,12 +20,12 @@ function Get-ConsulMetaServer
 
     # Go to the local consul node and get the address and the data center for the meta server
     $consulHttpUri = "$consulLocalAddress/v1/kv/environment/meta/http"
-    $consulHttpResponse = Invoke-WebRequest -Uri $consulHttpUri
+    $consulHttpResponse = Invoke-WebRequest -Uri $consulHttpUri -UseBasicParsing
     $json = ConvertFrom-Json -InputObject $consulHttpResponse
     $consulHttp = ConvertFrom-ConsulEncodedValue -encodedValue $json.Value
 
     $consulDataCenterUri = "$consulLocalAddress/v1/kv/environment/meta/datacenter"
-    $consulDataCenterResponse = Invoke-WebRequest -Uri $consulDataCenterUri
+    $consulDataCenterResponse = Invoke-WebRequest -Uri $consulDataCenterUri -UseBasicParsing
     $json = ConvertFrom-Json -InputObject $consulDataCenterResponse
     $consulDataCenter = ConvertFrom-ConsulEncodedValue -encodedValue $json.Value
 
@@ -51,37 +51,37 @@ function Get-ConsulTargetEnvironmentData
 
     # Get the name of the datacenter for our environment (e.g. the production environment is in the MyCompany-MyLocation01 datacenter)
     $consulDataCenterUri = "$($meta.Http)/v1/kv/environment/$lowerCaseEnvironment/datacenter?dc=$([System.Web.HttpUtility]::UrlEncode($meta.DataCenter))"
-    $consulDataCenterResponse = Invoke-WebRequest -Uri $consulDataCenterUri
+    $consulDataCenterResponse = Invoke-WebRequest -Uri $consulDataCenterUri -UseBasicParsing
     $json = ConvertFrom-Json -InputObject $consulDataCenterResponse
     $consulDataCenter = ConvertFrom-ConsulEncodedValue -encodedValue $json.Value
 
     # Get the http URL
     $consulHttpUri = "$($meta.Http)/v1/kv/environment/$lowerCaseEnvironment/http?dc=$([System.Web.HttpUtility]::UrlEncode($meta.DataCenter))"
-    $consulHttpResponse = Invoke-WebRequest -Uri $consulHttpUri
+    $consulHttpResponse = Invoke-WebRequest -Uri $consulHttpUri -UseBasicParsing
     $json = ConvertFrom-Json -InputObject $consulHttpResponse
     $consulHttp = ConvertFrom-ConsulEncodedValue -encodedValue $json.Value
 
     # Get the DNS URL
     $consulDnsUri = "$($meta.Http)/v1/kv/environment/$lowerCaseEnvironment/dns?dc=$([System.Web.HttpUtility]::UrlEncode($meta.DataCenter))"
-    $consulDnsResponse = Invoke-WebRequest -Uri $consulDnsUri
+    $consulDnsResponse = Invoke-WebRequest -Uri $consulDnsUri -UseBasicParsing
     $json = ConvertFrom-Json -InputObject $consulDnsResponse
     $consulDns = ConvertFrom-ConsulEncodedValue -encodedValue $json.Value
 
     # Get the serf_lan URL
     $consulSerfLanUri = "$($meta.Http)/v1/kv/environment/$lowerCaseEnvironment/serf_lan?dc=$([System.Web.HttpUtility]::UrlEncode($meta.DataCenter))"
-    $consulSerfLanResponse = Invoke-WebRequest -Uri $consulSerfLanUri
+    $consulSerfLanResponse = Invoke-WebRequest -Uri $consulSerfLanUri -UseBasicParsing
     $json = ConvertFrom-Json -InputObject $consulSerfLanResponse
     $consulSerfLan = ConvertFrom-ConsulEncodedValue -encodedValue $json.Value
 
     # Get the serf_wan URL
     $consulSerfWanUri = "$($meta.Http)/v1/kv/environment/$lowerCaseEnvironment/serf_wan?dc=$([System.Web.HttpUtility]::UrlEncode($meta.DataCenter))"
-    $consulSerfWanResponse = Invoke-WebRequest -Uri $consulSerfWanUri
+    $consulSerfWanResponse = Invoke-WebRequest -Uri $consulSerfWanUri -UseBasicParsing
     $json = ConvertFrom-Json -InputObject $consulSerfWanResponse
     $consulSerfWan = ConvertFrom-ConsulEncodedValue -encodedValue $json.Value
 
     # Get the server URL
     $consulServerUri = "$($meta.Http)/v1/kv/environment/$lowerCaseEnvironment/server?dc=$([System.Web.HttpUtility]::UrlEncode($meta.DataCenter))"
-    $consulServerResponse = Invoke-WebRequest -Uri $consulServerUri
+    $consulServerResponse = Invoke-WebRequest -Uri $consulServerUri -UseBasicParsing
     $json = ConvertFrom-Json -InputObject $consulServerResponse
     $consulServer = ConvertFrom-ConsulEncodedValue -encodedValue $json.Value
 
@@ -111,11 +111,62 @@ function Get-GlobalDnsAddress
 
     # Get the DNS server fallback URL
     $dnsFallbackUri = "$($meta.Http)/v1/kv/environment/$lowerCaseEnvironment/dns_fallback?dc=$([System.Web.HttpUtility]::UrlEncode($meta.DataCenter))"
-    $dnsFallbackResponse = Invoke-WebRequest -Uri $dnsFallbackUri
+    $dnsFallbackResponse = Invoke-WebRequest -Uri $dnsFallbackUri -UseBasicParsing
     $json = ConvertFrom-Json -InputObject $dnsFallbackResponse
     $dnsFallback = ConvertFrom-ConsulEncodedValue -encodedValue $json.Value
 
     return $dnsFallback
+}
+
+function Get-EnvironmentForLocalNode
+{
+    [CmdletBinding()]
+    param(
+        [string] $consulLocalAddress = "http://$($env:ComputerName):8500"
+    )
+
+    # Get the DC for the local node
+    $serviceUri = "$($consulLocalAddress)/v1/agent/self"
+    $serviceResponse = Invoke-WebRequest -Uri $serviceUri -UseBasicParsing
+    if ($serviceResponse.StatusCode -ne 200)
+    {
+        throw "Server did not return information about the local Consul node."
+    }
+
+    $json = ConvertFrom-Json -InputObject $serviceResponse
+    $dataCenter = $json.Config.Datacenter
+
+    # Go to the meta node and find out which DC belongs to which environment. Note that we're doing this the nasty way
+    # because we can't iterate over http addresses
+    $meta = Get-ConsulMetaServer -consulLocalAddress $consulLocalAddress
+    $consulDataCenterUri = "$($meta.Http)/v1/kv/environment/meta/datacenter?dc=$([System.Web.HttpUtility]::UrlEncode($meta.DataCenter))"
+    $consulDataCenterResponse = Invoke-WebRequest -Uri $consulDataCenterUri -UseBasicParsing
+    $json = ConvertFrom-Json -InputObject $consulDataCenterResponse
+    $metaDatacenter = ConvertFrom-ConsulEncodedValue -encodedValue $json.Value
+    if ($metaDataCenter -eq $dataCenter)
+    {
+        return 'meta'
+    }
+
+    $consulDataCenterUri = "$($meta.Http)/v1/kv/environment/production/datacenter?dc=$([System.Web.HttpUtility]::UrlEncode($meta.DataCenter))"
+    $consulDataCenterResponse = Invoke-WebRequest -Uri $consulDataCenterUri -UseBasicParsing
+    $json = ConvertFrom-Json -InputObject $consulDataCenterResponse
+    $metaDatacenter = ConvertFrom-ConsulEncodedValue -encodedValue $json.Value
+    if ($metaDataCenter -eq $dataCenter)
+    {
+        return 'production'
+    }
+
+    $consulDataCenterUri = "$($meta.Http)/v1/kv/environment/staging/datacenter?dc=$([System.Web.HttpUtility]::UrlEncode($meta.DataCenter))"
+    $consulDataCenterResponse = Invoke-WebRequest -Uri $consulDataCenterUri -UseBasicParsing
+    $json = ConvertFrom-Json -InputObject $consulDataCenterResponse
+    $metaDatacenter = ConvertFrom-ConsulEncodedValue -encodedValue $json.Value
+    if ($metaDataCenter -eq $dataCenter)
+    {
+        return 'staging'
+    }
+
+    return 'unknown'
 }
 
 function Get-ResourceNamesForService
@@ -136,7 +187,7 @@ function Get-ResourceNamesForService
         $serviceUri += "?tag=$([System.Web.HttpUtility]::UrlEncode($tag))"
     }
 
-    $serviceResponse = Invoke-WebRequest -Uri $serviceUri
+    $serviceResponse = Invoke-WebRequest -Uri $serviceUri -UseBasicParsing
     $json = ConvertFrom-Json -InputObject $serviceResponse
     $serviceAddress = $json[0].Address
 
@@ -156,7 +207,7 @@ function Get-ConsulKeyValue
 
     $keyUri = "$($server.Http)/v1/kv/$($keyPath)?dc=$([System.Web.HttpUtility]::UrlEncode($server.DataCenter))"
 
-    $keyResponse = Invoke-WebRequest -Uri $keyUri
+    $keyResponse = Invoke-WebRequest -Uri $keyUri -UseBasicParsing
     $json = ConvertFrom-Json -InputObject $keyResponse
     $value = ConvertFrom-ConsulEncodedValue -encodedValue $json.Value
 
