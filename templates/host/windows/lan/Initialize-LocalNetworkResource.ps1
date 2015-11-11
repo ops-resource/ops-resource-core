7c9ddce2faee56ce2766b8a6f98cf82fea8999e0
Petrik van der Velde <petrikvandervelde@gmail.com>
2015-11-11 14:25:48 +1300

    Passing on the credentials from the user.

--- templates/host/windows/lan/Initialize-LocalNetworkResource.ps1
+++ templates/host/windows/lan/Initialize-LocalNetworkResource.ps1
@@ -10,6 +10,16 @@
     The New-WindowsResource script takes all the actions necessary configure the remote machine.
 
 
+    .PARAMETER credential
+
+    The credential that should be used to connect to the remote machine.
+
+
+    .PARAMETER authenticateWithCredSSP
+
+    A flag that indicates whether remote powershell sessions should be authenticated with the CredSSP mechanism.
+
+
     .PARAMETER computerName
 
     The name of the machine that should be set up.
@@ -47,6 +57,12 @@
 [CmdletBinding()]
 param(
     [Parameter(Mandatory = $true)]
+    [PSCredential] $credential                                  = $(throw 'Please provide the credential object that should be used to connect to the remote machine.'),
+
+    [Parameter(Mandatory = $false)]
+    [switch] $authenticateWithCredSSP,
+
+    [Parameter(Mandatory = $true)]
     [string] $computerName = $(throw 'Please specify the name of the machine that should be configured.'),
 
     [Parameter(Mandatory = $true,
@@ -70,6 +86,8 @@ param(
     [string] $consulLocalAddress                                = "http://localhost:8500"
 )
 
+Write-Verbose "Initialize-LocalNetworkResource - credential: $credential"
+Write-Verbose "Initialize-LocalNetworkResource - authenticateWithCredSSP: $authenticateWithCredSSP"
 Write-Verbose "Initialize-LocalNetworkResource - computerName: $computerName"
 switch ($psCmdlet.ParameterSetName)
 {
@@ -112,6 +130,8 @@ try
     {
         'FromUserSpecification' {
             & $installationScript `
+                -credential $credential `
+                -authenticateWithCredSSP:$authenticateWithCredSSP `
                 -computerName $computerName `
                 -resourceName $resourceName `
                 -resourceVersion $resourceVersion `
@@ -126,6 +146,8 @@ try
 
         'FromMetaCluster' {
             & $installationScript `
+                -credential $credential `
+                -authenticateWithCredSSP:$authenticateWithCredSSP `
                 -computerName $computerName `
                 -resourceName $resourceName `
                 -resourceVersion $resourceVersion `
@@ -138,7 +160,13 @@ try
         }
     }
 
-    & $verificationScript -computerName $computerName -testDirectory $testDirectory -logDirectory $logDirectory @commonParameterSwitches
+    & $verificationScript `
+        -credential $credential `
+        -authenticateWithCredSSP:$authenticateWithCredSSP `
+        -computerName $computerName `
+        -testDirectory $testDirectory `
+        -logDirectory $logDirectory `
+        @commonParameterSwitches
 }
 finally
 {
