a25c0d9ad54c0728f85d56682de305bee0efbc4e
Petrik van der Velde <petrikvandervelde@gmail.com>
2015-12-07 23:38:30 +1300

    Moving the hyperv.ps1 script
    references #13

--- scripts/host/hyperv/windows/hyperv.ps1
+++ scripts/host/hyperv/windows/hyperv.ps1
@@ -1,21 +1,21 @@
 <#
     .SYNOPSIS
-    
+
     Gets the drive letter for the drive with the given drive number
-    
-    
+
+
     .DESCRIPTION
-    
+
     The Get-DriveLetter function returns the drive letter for the drive with the given drive number
-    
-    
+
+
     .PARAMETER driveNumber
-    
+
     The number of the drive.
-    
-    
+
+
     .OUTPUT
-    
+
     The letter of the drive.
 #>
 function Get-DriveLetter
@@ -25,56 +25,56 @@ function Get-DriveLetter
     param(
         [int] $driveNumber
     )
-    
+
     # The first drive is C which is ASCII 67
     return [char]($driveNumber + 67)
 }
 
 <#
     .SYNOPSIS
-    
+
     Creates a new Hyper-V virtual machine with the given properties.
-    
-    
+
+
     .DESCRIPTION
-    
+
     The New-HypervVm function creates a new Hyper-V virtual machine with the provided properties.
-    
-    
+
+
     .PARAMETER vmName
-    
+
     The name of the VM.
-    
-    
+
+
     .PARAMETER osVhdPath
-    
+
     The full path of the VHD that contains the pre-installed OS.
-    
-    
+
+
     .PARAMETER vmAdditionalDiskSizesInGb
-    
+
     An array containing the sizes, in Gb, of any additional VHDs that should be attached to the virtual machine.
-    
-    
+
+
     .PARAMETER vmNetworkSwitch
-    
+
     The name of the virtual network switch that the virtual machine should be connected to.
-    
-    
+
+
     .PARAMETER vmStoragePath
-    
+
     The full path of the directory where the virtual machine files should be stored.
-    
-    
+
+
     .PARAMETER vhdStoragePath
-    
+
     The full path of the directory where the virtual hard drive files should be stored.
-    
-    
+
+
     .EXAMPLE
     Example of how to use this cmdlet
-    
-    
+
+
     .EXAMPLE
     Another example of how to use this cmdlet
 #>
@@ -84,42 +84,45 @@ function New-HypervVm
     [OutputType([void])]
     Param
     (
+        [Parameter(Mandatory = $false)]
+        [string] $hypervHost = $env:COMPUTERNAME,
+
         [Parameter(Mandatory = $true)]
         [string] $vmName,
-        
+
         [Parameter(Mandatory = $true)]
         [string] $osVhdPath,
-        
+
         [Parameter(Mandatory = $true)]
         [int[]] $vmAdditionalDiskSizesInGb,
-        
+
         [Parameter(Mandatory = $true)]
         [string] $vmNetworkSwitch,
-        
+
         [Parameter(Mandatory = $true)]
         [string] $vmStoragePath,
-        
+
         [Parameter(Mandatory = $true)]
         [string] $vhdStoragePath
     )
-    
+
     Write-Verbose "New-HypervVm - vmName: $vmName"
-    Write-Verbose "New-HypervVm - osVhdPath $osVhdPath"
-    Write-Verbose "New-HypervVm - vmAdditionalDiskSizesInGb $vmAdditionalDiskSizesInGb"
-    Write-Verbose "New-HypervVm - vmNetworkSwitch $vmNetworkSwitch"
-    Write-Verbose "New-HypervVm - vmStoragePath $vmStoragePath"
-    Write-Verbose "New-HypervVm - vhdStoragePath $vhdStoragePath"
-    
+    Write-Verbose "New-HypervVm - osVhdPath: $osVhdPath"
+    Write-Verbose "New-HypervVm - vmAdditionalDiskSizesInGb: $vmAdditionalDiskSizesInGb"
+    Write-Verbose "New-HypervVm - vmNetworkSwitch: $vmNetworkSwitch"
+    Write-Verbose "New-HypervVm - vmStoragePath: $vmStoragePath"
+    Write-Verbose "New-HypervVm - vhdStoragePath: $vhdStoragePath"
+
     # Stop everything if there are errors
     $ErrorActionPreference = 'Stop'
-    
+
     $commonParameterSwitches =
         @{
             Verbose = $PSBoundParameters.ContainsKey('Verbose');
             Debug = $PSBoundParameters.ContainsKey('Debug');
             ErrorAction = 'Stop'
         }
-        
+
     $vm = New-Vm `
         -Name $vmName `
         -Path $vmStoragePath `
@@ -128,19 +131,20 @@ function New-HypervVm
         -SwitchName $vmNetworkSwitch `
         -Generation 2 `
         -BootDevice 'VHD' `
+        -ComputerName $hypervHost `
         @commonParameterSwitches
-        
-     $vm | 
+
+     $vm |
         Set-Vm `
             -ProcessorCount 1 `
             -Passthru `
-            @commonParameterSwitches 
-        
+            @commonParameterSwitches
+
     for ($i = 0; $i -lt $vmAdditionalDiskSizesInGb.Length; $i++)
     {
         $diskSize = $vmAdditionalDiskSizesInGb[$i]
-        
-        $driveLetter = Get-DriveLetter -driveNumber ($i + 1) 
+
+        $driveLetter = Get-DriveLetter -driveNumber ($i + 1)
         $path = Join-Path $vhdStoragePath "$($vmName)_$($driveLetter).vhdx"
         New-Vhd `
             -Path $path `
