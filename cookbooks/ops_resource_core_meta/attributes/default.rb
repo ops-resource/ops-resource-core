6f6ed1d4ec0acd7f8823c735d1b41c440dd5e395
Petrik van der Velde <petrikvandervelde@gmail.com>
2016-04-13 23:44:02 +1200

    Adding the meta cookbook
    references #15

---
+++ cookbooks/ops_resource_core_meta/attributes/default.rb
@@ -0,0 +1,5 @@
+logs_path = 'c:\\logs'
+default['paths']['log'] = logs_path
+
+meta_base_path = 'c:\\meta'
+default['paths']['meta'] = meta_base_path
---
+++ cookbooks/ops_resource_core_meta/metadata.rb
@@ -0,0 +1,7 @@
+name 'ops_resource_core_meta'
+maintainer '${CompanyName} (${CompanyUrl})'
+maintainer_email '${EmailDocumentation}'
+license 'All rights reserved'
+description 'Configures a server with the standard meta data information'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version '${VersionSemantic}'
