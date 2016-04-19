a6728906f4b20e3a68e7a4f277a37f00d18707d6
Petrik van der Velde <petrikvandervelde@gmail.com>
2016-04-19 22:25:32 +1200

    More ideas on how to get the configuration into the resource
    references #16

--- cookbooks/ops_resource_core_provisioning/files/windows/Initialize-Resource.ps1
+++ cookbooks/ops_resource_core_provisioning/files/windows/Initialize-Resource.ps1
@@ -7,11 +7,6 @@
     .DESCRIPTION
 
     The Initialize-Resource script executes all configuration and provisioning steps necessary to link the current resource to a given environment.
-
-
-    .EXAMPLE
-
-    Install-ApplicationsOnWindowsWithChef -configurationDirectory "c:\temp\configuration" -logDirectory "c:\temp\logs" -cookbookNames "myCookbook", "myOtherCookbook"
 #>
 [Cmdletbinding()]
 param(
@@ -19,11 +14,20 @@ param(
 
 # READ CONFIGURATION FILE
 
+# -> Configuration file has single URI
+#    -> Call get on that URI to
+
+
+# Get URL
+
+# send request to URL. Request should contain:
+# - container ID: e.g. machine MAC, container ID, etc. etc.
+# - resource ID: e.g. ops-resource-core, webserver etc.
 
 
 
 # GET CONFIGURATION FROM CONFIGURATION HOST
-#
+
 
 
 # INVOKE ALL POWERSHELL FILES IN THE PROVISIONING DIRECTORY THAT ARE NAMED: Initialize-XXXResource
