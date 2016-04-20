fd6ad6e906c788c8db37cc27f0387551eec9218a
Petrik van der Velde <petrikvandervelde@gmail.com>
2016-04-20 23:31:17 +1200

    Working on the configuration / provisioning script
    references #16

--- cookbooks/ops_resource_core_provisioning/files/windows/Initialize-Resource.ps1
+++ cookbooks/ops_resource_core_provisioning/files/windows/Initialize-Resource.ps1
@@ -7,28 +7,106 @@
     .DESCRIPTION
 
     The Initialize-Resource script executes all configuration and provisioning steps necessary to link the current resource to a given environment.
+    The configuration information is obtained by sending a GET request to a URI specified either in a custom
+    JSON file in '<HOMEDRIVE>:\provisioning\provisioning.json' or by getting the content of the ProvisioningEntryPoint
+    environment variable.
+
+    It is expected that the JSON file has the following elements:
+
+        {
+            "entrypoint": "http://example.com/provisioning"
+        }
 #>
 [Cmdletbinding()]
 param(
 )
 
-# READ CONFIGURATION FILE
+# Stop everything if there are errors
+$ErrorActionPreference = 'Stop'
 
-# -> Configuration file has single URI
-#    -> Call get on that URI to
+$commonParameterSwitches =
+    @{
+        Verbose = $PSBoundParameters.ContainsKey('Verbose');
+        Debug = $false;
+        ErrorAction = "Stop"
+    }
 
+try
+{
+    $provisioningBaseUri = ''
 
-# Get URL
+    $expectedConfigurationFile = Join-Path $env:HOMEDRIVE 'provisioning\provisioning.json'
+    if (Test-Path $expectedConfigurationFile)
+    {
+        # Read configuration file
+        $content = Get-Content -Path $expectedConfigurationFile @commonParameterSwitches
+        $json = ConvertFrom-Json $content
+        $provisioningBaseUri = $json.entrypoint
+    }
+    else
+    {
+        $provisioningBaseUri = $env:ProvisioningEntryPoint
+    }
 
-# send request to URL. Request should contain:
-# - container ID: e.g. machine MAC, container ID, etc. etc.
-# - resource ID: e.g. ops-resource-core, webserver etc.
+    if (($provisioningBaseUri -eq $null) -or ($provisioningBaseUri -eq ''))
+    {
+        throw
+    }
 
+    # send request to URL. Request should contain:
+    # - container ID: e.g. machine MAC, container ID, etc. etc.
+    # - resource ID: e.g. ops-resource-core, webserver etc.
+    $body = ""
+    $response = Invoke-WebRequest `
+        -Uri $provisioningBaseUri `
+        -Method Get `
+        -Body $body `
+        -ContentType 'application/json' `
+        -UseDefaultCredentials `
+        -UseBasicParsing `
+        @commonParameterSwitches
 
+    if ($response.StatusCode -ne 200)
+    {
+        Write-Error "Failed to get configuration data from server. Response was $($response.StatusCode)"
+    }
 
-# GET CONFIGURATION FROM CONFIGURATION HOST
+    $json = ConvertFrom-Json -InputObject $response.Content @commonParameterSwitches
 
+    $scriptPath = Split-Path -Path $PSScriptRoot -Parent @commonParameterSwitches
+    $scriptsToExecute = Get-ChildItem -Path $scriptPath -Filter 'Initialize-*Resource.ps1' -File
+    foreach($script in $scriptsToExecute)
+    {
+        try
+        {
+            & $script `
+                @commonParameterSwitches
+        }
+        catch
+        {
 
+        }
+    }
+}
+finally
+{
+    try
+    {
+        Set-Service `
+            -Name 'Provisioning' `
+            -StartupType Disabled `
+            @commonParameterSwitches
 
-# INVOKE ALL POWERSHELL FILES IN THE PROVISIONING DIRECTORY THAT ARE NAMED: Initialize-XXXResource
+        Stop-Service `
+            -Name 'Provisioning' `
+            -NoWait `
+            -Force `
+            @commonParameterSwitches
+    }
+    catch
+    {
+        Write-Error "Failed to stop the service. Error was $($_.Exception.ToString())"
+    }
+}
+
 
