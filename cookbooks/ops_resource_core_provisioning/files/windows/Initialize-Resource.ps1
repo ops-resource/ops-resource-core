627744216b4f0b56c2d81b700b5f73c581a780d7
Petrik van der Velde <petrikvandervelde@gmail.com>
2016-04-21 23:44:57 +1200

    More getting the configuration
    references #16

--- cookbooks/ops_resource_core_provisioning/files/windows/Initialize-Resource.ps1
+++ cookbooks/ops_resource_core_provisioning/files/windows/Initialize-Resource.ps1
@@ -31,8 +31,94 @@ $commonParameterSwitches =
         ErrorAction = "Stop"
     }
 
-try
+# -------------------------- Script functions --------------------------------
+
+<#
+    .SYNOPSIS
+
+    Gets information describing the configuration of the current resource
+
+
+    .DESCRIPTION
+
+    The Get-ConfigurationInformation function gets information that describes the configuration of the current resource
+
+
+    .OUTPUTS
+
+    A custom object describing the configuration. The object is expected to have the following properties:
+
+        Name
+        ID
+        ConfigurationUri
+#>
+function Get-ConfigurationInformation
 {
+    [CmdletBinding()]
+    param(
+        [string[]] $resourceNames
+    )
+
+    # send request to URL. Request should contain:
+    # - container ID: e.g. machine MAC, container ID, etc. etc.
+    # - resource ID: e.g. ops-resource-core, webserver etc.
+    $configurationRequest = New-ConfigurationRequest `
+        @commonParameterSwitches
+
+    $body = ConvertTo-Json $configurationRequest
+    $response = Invoke-WebRequest `
+        -Uri $environmentInformation.ConfigurationUri `
+        -Method Get `
+        -Body $body `
+        -ContentType 'application/json' `
+        -UseDefaultCredentials `
+        -UseBasicParsing `
+        @commonParameterSwitches
+
+    if ($response.StatusCode -ne 200)
+    {
+        Write-Error "Failed to get configuration data from server. Response was $($response.StatusCode)"
+    }
+
+    $json = ConvertFrom-Json -InputObject $response.Content @commonParameterSwitches
+}
+
+<#
+    .SYNOPSIS
+
+    Gets information describing the environment that the current resource should be connected to.
+
+
+    .DESCRIPTION
+
+    The Get-EnvironmentInformation function gets the information that describes the environment to which the current resource should be connected.
+
+
+    .OUTPUTS
+
+    A custom object describing the environment. The object is expected to have the following properties:
+
+        Name
+        ID
+        ConfigurationUri
+#>
+function Get-EnvironmentInformation
+{
+    [CmdletBinding()]
+    param(
+
+    )
+
+    # Stop everything if there are errors
+    $ErrorActionPreference = 'Stop'
+
+    $commonParameterSwitches =
+        @{
+            Verbose = $PSBoundParameters.ContainsKey('Verbose');
+            Debug = $false;
+            ErrorAction = "Stop"
+        }
+
     $provisioningBaseUri = ''
 
     $expectedConfigurationFile = Join-Path $env:HOMEDRIVE 'provisioning\provisioning.json'
@@ -53,10 +139,10 @@ try
         throw
     }
 
-    # send request to URL. Request should contain:
-    # - container ID: e.g. machine MAC, container ID, etc. etc.
-    # - resource ID: e.g. ops-resource-core, webserver etc.
-    $body = ""
+    $environmentRequest = New-EnvironmentRequest `
+        @commonParameterSwitches
+
+    $body = ConvertTo-Json $environmentRequest
     $response = Invoke-WebRequest `
         -Uri $provisioningBaseUri `
         -Method Get `
@@ -73,8 +159,98 @@ try
 
     $json = ConvertFrom-Json -InputObject $response.Content @commonParameterSwitches
 
+    # object that contains:
+    # - Environment name
+    # - Environment ID
+    # - Uri / IP of the configuration server for the environment
+}
+
+function New-ConfigurationRequest
+{
+    [CmdletBinding()]
+    param()
+
+    # Stop everything if there are errors
+    $ErrorActionPreference = 'Stop'
+
+    $commonParameterSwitches =
+        @{
+            Verbose = $PSBoundParameters.ContainsKey('Verbose');
+            Debug = $false;
+            ErrorAction = "Stop"
+        }
+
+    $result = New-Object psobject
+    Add-Member -InputObject $result -MemberType NoteProperty -Name MachineId -Value ''
+}
+
+<#
+    .SYNOPSIS
+
+    Gets the MAC and IP addresses for all active network adapters.
+
+
+    .DESCRIPTION
+
+    The New-MachineIdentifiers function gets the information that describes the machine.
+
+
+    .OUTPUTS
+
+    An array containing custom objects that store the MacAddress and the IP address for all active network adapters on the machine.
+#>
+function New-MachineIdentifiers
+{
+    [CmdletBinding()]
+    param()
+
+    $ErrorActionPreference = 'Stop'
+
+    $commonParameterSwitches =
+        @{
+            Verbose = $PSBoundParameters.ContainsKey('Verbose');
+            Debug = $false;
+            ErrorAction = "Stop"
+        }
+
+    $info = @()
+    $adapters = Get-NetAdapter @commonParameterSwitches
+    foreach($adapter in $adapters)
+    {
+        if ($adapter.Status -ne 'Up')
+        {
+            continue
+        }
+
+        $address = Get-NetIPAddress -InterfaceAlias $adapter.InterfaceAlias |
+            Where-Object { $_.AddressFamily -ne 'IPv6' }
+
+        if (($address -ne $null) -and ($address -ne ''))
+        {
+            $result = New-Object psobject
+            Add-Member -InputObject $result -MemberType NoteProperty -Name MacAddress -Value $adapter.MacAddress
+            Add-Member -InputObject $result -MemberType NoteProperty -Name IPAddresses -Value $address
+
+            $info += $result
+        }
+    }
+
+    return $info
+}
+
+# -------------------------- Script start ------------------------------------
+
+try
+{
+    $environmentInformation = Get-EnvironmentInformation `
+        @commonParameterSwitches
+
     $scriptPath = Split-Path -Path $PSScriptRoot -Parent @commonParameterSwitches
     $scriptsToExecute = Get-ChildItem -Path $scriptPath -Filter 'Initialize-*Resource.ps1' -File
+
+    $configurationInformation = Get-ConfigurationInformation `
+        -resourceNames '' `
+        @commonParameterSwitches
     foreach($script in $scriptsToExecute)
     {
         try
