ac2f41a3ca12cd587fd571c46f2810321ada73e1
Petrik van der Velde <petrikvandervelde@gmail.com>
2016-04-25 22:25:06 +1200

    Allowing the installation of a consul agent or a consul server.
    references #17

--- cookbooks/ops_resource_core_consul/recipes/consul.rb
+++ cookbooks/ops_resource_core_consul/recipes/consul.rb
@@ -10,12 +10,7 @@
 include_recipe 'windows'
 include_recipe 'windows_firewall'
 
-log_directory = node['paths']['log']
-directory log_directory do
-  rights :read, 'Everyone', applies_to_children: true
-  rights :modify, 'Administrators', applies_to_children: true
-  action :create
-end
+Chef::Recipe.send(:include, Consul::Helper)
 
 service_name = node['service']['consul']
 win_service_name = 'consul_service'
@@ -107,6 +102,21 @@ powershell_script 'user_grant_service_logon_rights' do
   POWERSHELL
 end
 
+# CONFIGURE LOG DIRECTORIES
+log_directory = node['paths']['log']
+directory log_directory do
+  rights :read, 'Everyone', applies_to_children: true
+  rights :modify, 'Administrators', applies_to_children: true
+  action :create
+end
+
+consul_logs_directory = node['paths']['consul_logs']
+directory consul_logs_directory do
+  rights :modify, service_username, applies_to_children: true, applies_to_self: false
+  action :create
+end
+
+# CONFIGURE CONSUL DIRECTORIES
 ops_base_directory = node['paths']['ops_base']
 directory ops_base_directory do
   rights :read, 'Everyone', applies_to_children: true
@@ -125,12 +135,7 @@ directory consul_data_directory do
   action :create
 end
 
-consul_logs_directory = node['paths']['consul_logs']
-directory consul_logs_directory do
-  rights :modify, service_username, applies_to_children: true, applies_to_self: false
-  action :create
-end
-
+# CONFIGURE CONSUL CONFIG DIRECTORIES
 consul_config_directory = node['paths']['consul_config']
 directory consul_config_directory do
   action :create
@@ -147,6 +152,7 @@ directory consul_checks_directory do
   action :create
 end
 
+# CONFIGURE CONSUL EXECUTABLE
 consul_bin_directory = node['paths']['consul_bin']
 directory consul_bin_directory do
   rights :read_execute, 'Everyone', applies_to_children: true, applies_to_self: false
@@ -159,15 +165,73 @@ cookbook_file "#{consul_bin_directory}\\#{consul_exe}" do
   action :create
 end
 
+windows_firewall_rule 'Consul_TCP' do
+  dir 'in'
+  firewall_action :allow
+  protocol 'TCP'
+  program "#{consul_bin_directory}\\consul.exe"
+  profile 'domain'
+  action :create
+end
+
+windows_firewall_rule 'Consul_UDP' do
+  dir 'in'
+  firewall_action :allow
+  protocol 'UDP'
+  program "#{consul_bin_directory}\\consul.exe"
+  profile 'domain'
+  action :create
+end
+
 # Getting a comma separed set of IP addresses that are the IP addresses of the DNS recusors
 # For the consul configuration we want a formatted string that looks like:
 # "recursor_IP_1","recursor_IP_2","recursor_IP_3"
-recusors_formatted = node['env_external']['dns_server'].gsub(',', '\",\"')
+recursors_formatted = node['env_external']['dns_server'].gsub(',', '\",\"')
 consul_config_recursors = "\"#{recursors_formatted}\""
 
-consul_config_datacenter = node['consul']['datacenter']
-consul_config_entry_node_dns = node['consul']['entry_node_dns']
-consul_config_recursors = node['consul']['dns_server_url']
+environment = 'env_consul'
+datacenter = node[environment]['consul_datacenter']
+dns_port = node[environment]['consul_dns_port']
+http_port = node[environment]['consul_http_port']
+rpc_port = node[environment]['consul_rpc_port']
+serf_lan_port = node[environment]['consul_serf_lan_port']
+serf_wan_port = node[environment]['consul_serf_wan_port']
+server_port = node[environment]['consul_server_port']
+
+consul_server = ''
+consul_addresses = ''
+retry_join_wan = ''
+retry_join_lan = ''
+if node[environment]['consul_as_server'].casecmp 'true'
+  numberofservers = node[environment]['consul_server_count']
+  consuldomain = node[environment]['consul_domain']
+
+  consul_server = <<-TEXT
+  "bootstrap_expect" : #{numberofservers},
+  "server": true,
+  "domain": "#{consuldomain}",
+  TEXT
+
+  machine_ip = local_ip
+  consul_addresses = <<-TEXT
+  "addresses": {
+    "dns": "#{machine_ip}"
+  },
+  TEXT
+
+  consul_retry_join_wan_nodes = node[environment]['wan_server_node_dns'].gsub(',', '\",\"')
+  retry_join_wan = <<-TEXT
+  "retry_join_wan": ["#{consul_retry_join_wan_nodes}"],
+  "retry_interval_wan": "30s",
+  TEXT
+else
+  consul_retry_join_lan_nodes = node[environment]['lan_server_node_dns'].gsub(',', '\",\"')
+  retry_join_lan = <<-TEXT
+  "retry_join": ["#{consul_retry_join_lan_nodes}"],
+  "retry_interval": "30s",
+  TEXT
+
+end
 
 consul_config_file = 'consul_default.json'
 # We need to multiple-escape the escape character because of ruby string and regex etc. etc. See here: http://stackoverflow.com/a/6209532/539846
@@ -177,10 +241,18 @@ file "#{consul_bin_directory}\\#{consul_config_file}" do
 {
   "data_dir": "#{consul_data_directory_json_escaped}",
 
-  "datacenter": "#{consul_config_datacenter}",
+#{consul_server}
 
+  "datacenter": "#{datacenter}",
+
+#{consul_addresses}
   "ports": {
-    "dns": 53
+    "dns": #{dns_port}
+    "http": #{http_port},
+    "rpc": #{rpc_port},
+    "serf_lan": #{serf_lan_port},
+    "serf_wan": #{serf_wan_port},
+    "server": #{server_port}
   },
 
   "dns_config" : {
@@ -192,9 +264,8 @@ file "#{consul_bin_directory}\\#{consul_config_file}" do
     }
   },
 
-  "retry_join": ["#{consul_config_entry_node_dns}"],
-  "retry_interval": "30s",
-
+#{retry_join_lan}
+#{retry_join_wan}
   "recursors": [#{consul_config_recursors}],
 
   "disable_remote_exec": true,
@@ -205,8 +276,7 @@ file "#{consul_bin_directory}\\#{consul_config_file}" do
   JSON
 end
 
-# add the winsw binaries
-# Copy the service runner & rename to consul.exe
+# INSTALL CONSUL AS SERVICE
 cookbook_file "#{consul_bin_directory}\\#{win_service_name}.exe" do
   source 'winsw.exe'
   action :create
@@ -259,7 +329,6 @@ file "#{consul_bin_directory}\\#{win_service_name}.xml" do
   action :create
 end
 
-# Install consul_service.exe as service
 powershell_script 'consul_as_service' do
   code <<-POWERSHELL
     $ErrorActionPreference = 'Stop'
@@ -297,7 +366,7 @@ registry_key "HKEY_LOCAL_MACHINE\\SYSTEM\\CurrentControlSet\\services\\eventlog\
   action :create
 end
 
-# Add file to meta directory containing information about consul install
+# STORE META INFORMATION
 meta_directory = node['paths']['meta']
 consul_bin_directory_escaped = consul_bin_directory.gsub('\\', '\\\\\\\\')
 consul_config_directory_escaped = consul_config_directory.gsub('\\', '\\\\\\\\')
@@ -309,22 +378,4 @@ file "#{meta_directory}\\service_consul.json" do
 }
   JSON
   action :create
-end
-
-windows_firewall_rule 'Consul_TCP' do
-    dir 'in'
-    firewall_action :allow
-    protocol 'TCP'
-    program "#{consul_bin_directory}\\consul.exe"
-    profile 'domain'
-    action :create
-end
-
-windows_firewall_rule 'Consul_UDP' do
-    dir 'in'
-    firewall_action :allow
-    protocol 'UDP'
-    program "#{consul_bin_directory}\\consul.exe"
-    profile 'domain'
-    action :create
 end
--- cookbooks/ops_resource_core_consul/spec/consul_spec.rb
+++ cookbooks/ops_resource_core_consul/spec/consul_spec.rb
@@ -1,4 +1,6 @@
 require 'chefspec'
+require 'socket'
+require_relative '../libraries/consul_helper'
 
 RSpec.configure do |config|
   # Specify the path for Chef Solo to find cookbooks (default: [inferred from
@@ -25,108 +27,113 @@ RSpec.configure do |config|
 end
 
 describe 'ops_resource_core_consul::consul' do
-  let(:chef_run) { ChefSpec::SoloRunner.converge(described_recipe) }
-
   logs_path = 'c:\\logs'
-  it 'creates the logs base directory' do
-    expect(chef_run).to create_directory(logs_path)
-  end
-
-  it 'creates the consul user' do
-    expect(chef_run).to create_user('consul_user')
-    expect(chef_run).to modify_group('Performance Monitor Users').with(members: ['consul_user'])
-  end
-
-  ops_base_path = 'c:\\ops'
-  it 'creates the ops base directory' do
-    expect(chef_run).to create_directory(ops_base_path)
-  end
-
-  consul_base_path = 'c:\\ops\\consul'
-  it 'creates the consul base directory' do
-    expect(chef_run).to create_directory(consul_base_path)
-  end
-
-  consul_data_directory = 'c:\\ops\\consul\\data'
-  it 'creates the consul data directory' do
-    expect(chef_run).to create_directory(consul_data_directory)
-  end
-
   consul_logs_directory = 'c:\\logs\\consul'
-  it 'creates the consul logs directory' do
-    expect(chef_run).to create_directory(consul_logs_directory)
-  end
 
   meta_directory = 'c:\\meta'
   consul_config_directory = 'c:\\meta\\consul'
-  it 'creates the consul config directory' do
-    expect(chef_run).to create_directory(consul_config_directory)
-  end
-
-  consul_config_upload_file = 'Set-ConfigurationInConsulCluster.ps1'
-  it 'creates Set-ConfigurationInConsulCluster.ps1 in the consul config directory' do
-    expect(chef_run).to create_cookbook_file("#{consul_config_directory}\\#{consul_config_upload_file}").with_source(consul_config_upload_file)
-  end
-
   consul_checks_directory = 'c:\\meta\\consul\\checks'
-  it 'creates the consul checks directory' do
-    expect(chef_run).to create_directory(consul_checks_directory)
-  end
 
+  ops_base_path = 'c:\\ops'
+  consul_base_path = 'c:\\ops\\consul'
+  consul_data_directory = 'c:\\ops\\consul\\data'
   consul_bin_directory = 'c:\\ops\\consul\\bin'
-  it 'creates the consul bin directory' do
-    expect(chef_run).to create_directory(consul_bin_directory)
-  end
 
   service_name = 'consul'
-  it 'creates consul.exe in the consul ops directory' do
-    expect(chef_run).to create_cookbook_file("#{consul_bin_directory}\\#{service_name}.exe").with_source("#{service_name}.exe")
-  end
-
-  consul_config_datacenter = '${ConsulDataCenterName}'
-  consul_config_entry_node_dns = '${ConsulClusterEntryPointAddress}'
-  consul_config_recursors = '"${ConsulGlobalDnsServerAddress}"'
-  consul_default_config_content = <<-JSON
-{
-  "data_dir": "c:\\\\ops\\\\consul\\\\data",
-
-  "datacenter": "#{consul_config_datacenter}",
-
-  "ports": {
-    "dns": 53
-  },
-
-  "dns_config" : {
-    "allow_stale" : true,
-    "max_stale" : "150s",
-    "node_ttl" : "300s",
-    "service_ttl": {
-      "*": "300s"
-    }
-  },
-
-  "retry_join": ["#{consul_config_entry_node_dns}"],
-  "retry_interval": "30s",
-
-  "recursors": [#{consul_config_recursors}],
-
-  "disable_remote_exec": true,
-  "disable_update_check": true,
-
-  "log_level" : "debug"
-}
-  JSON
   consul_config_file = 'consul_default.json'
-  it 'creates consul_default.json in the consul ops directory' do
-    expect(chef_run).to create_file("#{consul_bin_directory}\\#{consul_config_file}").with_content(consul_default_config_content)
+  context 'create the log locations' do
+    let(:chef_run) { ChefSpec::SoloRunner.converge(described_recipe) }
+
+    it 'creates the logs base directory' do
+      expect(chef_run).to create_directory(logs_path)
+    end
+
+    it 'creates the consul logs directory' do
+      expect(chef_run).to create_directory(consul_logs_directory)
+    end
   end
 
-  win_service_name = 'consul_service'
-  it 'creates consul_service.exe in the consul ops directory' do
-    expect(chef_run).to create_cookbook_file("#{consul_bin_directory}\\#{win_service_name}.exe").with_source('winsw.exe')
+  context 'create the meta locations' do
+    let(:chef_run) { ChefSpec::SoloRunner.converge(described_recipe) }
+
+    it 'creates the consul config directory' do
+      expect(chef_run).to create_directory(consul_config_directory)
+    end
+
+    consul_config_upload_file = 'Set-ConfigurationInConsulCluster.ps1'
+    it 'creates Set-ConfigurationInConsulCluster.ps1 in the consul config directory' do
+      expect(chef_run).to create_cookbook_file("#{consul_config_directory}\\#{consul_config_upload_file}").with_source(consul_config_upload_file)
+    end
+
+    it 'creates the consul checks directory' do
+      expect(chef_run).to create_directory(consul_checks_directory)
+    end
   end
 
-  consul_service_exe_config_content = <<-XML
+  context 'create the consul locations' do
+    let(:chef_run) { ChefSpec::SoloRunner.converge(described_recipe) }
+
+    it 'creates the ops base directory' do
+      expect(chef_run).to create_directory(ops_base_path)
+    end
+
+    it 'creates the consul base directory' do
+      expect(chef_run).to create_directory(consul_base_path)
+    end
+
+    it 'creates the consul data directory' do
+      expect(chef_run).to create_directory(consul_data_directory)
+    end
+
+    it 'creates the consul bin directory' do
+      expect(chef_run).to create_directory(consul_bin_directory)
+    end
+
+    it 'creates consul.exe in the consul ops directory' do
+      expect(chef_run).to create_cookbook_file("#{consul_bin_directory}\\#{service_name}.exe").with_source("#{service_name}.exe")
+    end
+
+    # it 'opens the TCP ports for consul in the firewall' do
+    #   expect(chef_run).to create_windows_firewall_rule('Consul_TCP').with(
+    #     values: [{
+    #       dir: 'in',
+    #       firewall_action: :allow,
+    #       protocol: 'TCP',
+    #       program: "#{consul_bin_directory}\\consul.exe",
+    #       profile: 'domain'
+    #     }])
+    # end
+
+    # it 'opens the UDP ports for consul in the firewall' do
+    #   expect(chef_run).to create_windows_firewall_rule('Consul_UDP').with(
+    #     values: [{
+    #       dir: 'in',
+    #       firewall_action: :allow,
+    #       protocol: 'UDP',
+    #       program: "#{consul_bin_directory}\\consul.exe",
+    #       profile: 'domain'
+    #     }])
+    # end
+  end
+
+  context 'create the user to run the service with' do
+    let(:chef_run) { ChefSpec::SoloRunner.converge(described_recipe) }
+
+    it 'creates the consul user' do
+      expect(chef_run).to create_user('consul_user')
+      expect(chef_run).to modify_group('Performance Monitor Users').with(members: ['consul_user'])
+    end
+  end
+
+  context 'install consul as service' do
+    let(:chef_run) { ChefSpec::SoloRunner.converge(described_recipe) }
+
+    win_service_name = 'consul_service'
+    it 'creates consul_service.exe in the consul ops directory' do
+      expect(chef_run).to create_cookbook_file("#{consul_bin_directory}\\#{win_service_name}.exe").with_source('winsw.exe')
+    end
+
+    consul_service_exe_config_content = <<-XML
 <configuration>
     <runtime>
         <generatePublisherEvidence enabled="false"/>
@@ -136,13 +143,13 @@ describe 'ops_resource_core_consul::consul' do
         <supportedRuntime version="v2.0.50727" />
     </startup>
 </configuration>
-  XML
-  it 'creates consul_service.exe.config in the consul ops directory' do
-    expect(chef_run).to create_file("#{consul_bin_directory}\\#{win_service_name}.exe.config").with_content(consul_service_exe_config_content)
-  end
+    XML
+    it 'creates consul_service.exe.config in the consul ops directory' do
+      expect(chef_run).to create_file("#{consul_bin_directory}\\#{win_service_name}.exe.config").with_content(consul_service_exe_config_content)
+    end
 
-  consul_service_xml_content = <<-XML
-<?xml version="1.0"?>
+    consul_service_xml_content = <<-XML
+    <?xml version="1.0"?>
 <!--
     The MIT License Copyright (c) 2004-2009, Sun Microsystems, Inc., Kohsuke Kawaguchi Permission is hereby granted, free of charge, to any person obtaining a
     copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights
@@ -168,53 +175,164 @@ describe 'ops_resource_core_consul::consul' do
     </log>
     <onfailure action="restart"/>
 </service>
-  XML
-  it 'creates consul_service.xml in the consul ops directory' do
-    expect(chef_run).to create_file("#{consul_bin_directory}\\#{win_service_name}.xml").with_content(consul_service_xml_content)
+    XML
+    it 'creates consul_service.xml in the consul ops directory' do
+      expect(chef_run).to create_file("#{consul_bin_directory}\\#{win_service_name}.xml").with_content(consul_service_xml_content)
+    end
+
+    it 'installs consul as service' do
+      expect(chef_run).to run_powershell_script('consul_as_service')
+    end
+
+    it 'creates the windows service event log' do
+      expect(chef_run).to create_registry_key("HKEY_LOCAL_MACHINE\\SYSTEM\\CurrentControlSet\\services\\eventlog\\Application\\#{service_name}").with(
+        values: [{
+          name: 'EventMessageFile',
+          type: :string,
+          data: 'c:\\Windows\\Microsoft.NET\\Framework64\\v4.0.30319\\EventLogMessages.dll'
+        }])
+    end
   end
 
-  it 'installs consul as service' do
-    expect(chef_run).to run_powershell_script('consul_as_service')
+  context 'install consul as client' do
+    let(:chef_run) do
+      node.set['env_consul']['consul_as_server'] = false
+      node.set['env_consul']['consul_datacenter'] = 'MyDatacenter'
+      node.set['env_consul']['consul_dns_port'] = 1
+      node.set['env_consul']['consul_http_port'] = 2
+      node.set['env_consul']['consul_rpc_port'] = 3
+      node.set['env_consul']['consul_serf_lan_port'] = 4
+      node.set['env_consul']['consul_serf_wan_port'] = 5
+      node.set['env_consul']['consul_server_port'] = 6
+      node.set['env_external']['dns_server'] = '4.3.2.1,3.2.1.0'
+      node.set['env_consul']['lan_server_node_dns'] = '5.4.3.2,6.5.4.3,7.6.5.4'
+      node.set['env_consul']['consul_server_count'] = -1
+      node.set['env_consul']['consul_domain'] = 'CONSUL_DOMAIN_NOT_SET'
+      node.set['env_consul']['wan_server_node_dns'] = 'WAN_DNS_NOT_SET'
+      ChefSpec::SoloRunner.converge(described_recipe)
+    end
+
+    consul_default_config_content = <<-JSON
+{
+  "data_dir": "c:\\\\ops\\\\consul\\\\data",
+
+
+
+  "datacenter": "MyDatacenter",
+
+
+  "ports": {
+    "dns": 1
+    "http": 2,
+    "rpc": 3,
+    "serf_lan": 4,
+    "serf_wan": 5,
+    "server": 6
+  },
+
+  "dns_config" : {
+    "allow_stale" : true,
+    "max_stale" : "150s",
+    "node_ttl" : "300s",
+    "service_ttl": {
+      "*": "300s"
+    }
+  },
+
+  "retry_join": ["5.4.3.2","6.5.4.3","7.6.5.4"],
+  "retry_interval": "30s",
+
+  "recursors": ["4.3.2.1","3.2.1.0"],
+
+  "disable_remote_exec": true,
+  "disable_update_check": true,
+
+  "log_level" : "debug"
+}
+    JSON
+    it 'creates consul_default.json in the consul ops directory' do
+      expect(chef_run).to create_file("#{consul_bin_directory}\\#{consul_config_file}").with_content(consul_default_config_content)
+    end
   end
 
-  it 'creates the windows service event log' do
-    expect(chef_run).to create_registry_key("HKEY_LOCAL_MACHINE\\SYSTEM\\CurrentControlSet\\services\\eventlog\\Application\\#{service_name}").with(
-      values: [{
-        name: 'EventMessageFile',
-        type: :string,
-        data: 'c:\\Windows\\Microsoft.NET\\Framework64\\v4.0.30319\\EventLogMessages.dll'
-      }])
+  context 'install consul as server' do
+    let(:chef_run) do
+      node.set['env_consul']['consul_as_server'] = true
+      node.set['env_consul']['consul_datacenter'] = 'MyDatacenter'
+      node.set['env_consul']['consul_dns_port'] = 1
+      node.set['env_consul']['consul_http_port'] = 2
+      node.set['env_consul']['consul_rpc_port'] = 3
+      node.set['env_consul']['consul_serf_lan_port'] = 4
+      node.set['env_consul']['consul_serf_wan_port'] = 5
+      node.set['env_consul']['consul_server_port'] = 6
+      node.set['env_external']['dns_server'] = '4.3.2.1,3.2.1.0'
+      node.set['env_consul']['lan_server_node_dns'] = 'LAN_DNS_NOT_SET'
+      node.set['env_consul']['consul_server_count'] = 7
+      node.set['env_consul']['consul_domain'] = 'MyDomain'
+      node.set['env_consul']['wan_server_node_dns'] = '5.4.3.2,6.5.4.3,7.6.5.4'
+      ChefSpec::SoloRunner.converge(described_recipe)
+    end
+
+    machine_ip = Consul::Helper.local_ip
+
+    consul_default_config_content = <<-JSON
+{
+  "data_dir": "c:\\\\ops\\\\consul\\\\data",
+
+  "bootstrap_expect" : 7,
+  "server": true,
+  "domain": "MyDomain",
+
+  "datacenter": "MyDatacenter",
+
+  "addresses": {
+    "dns": "#{machine_ip}"
+  },
+  "ports": {
+    "dns": 1
+    "http": 2,
+    "rpc": 3,
+    "serf_lan": 4,
+    "serf_wan": 5,
+    "server": 6
+  },
+
+  "dns_config" : {
+    "allow_stale" : true,
+    "max_stale" : "150s",
+    "node_ttl" : "300s",
+    "service_ttl": {
+      "*": "300s"
+    }
+  },
+
+
+  "retry_join_wan": ["5.4.3.2","6.5.4.3","7.6.5.4"],
+  "retry_interval_wan": "30s",
+  "recursors": ["4.3.2.1","3.2.1.0"],
+
+  "disable_remote_exec": true,
+  "disable_update_check": true,
+
+  "log_level" : "debug"
+}
+    JSON
+    it 'creates consul_default.json in the consul ops directory' do
+      expect(chef_run).to create_file("#{consul_bin_directory}\\#{consul_config_file}").with_content(consul_default_config_content)
+    end
   end
 
-  consul_service_config_content = <<-JSON
+  context 'store the meta' do
+    let(:chef_run) { ChefSpec::SoloRunner.converge(described_recipe) }
+
+    consul_service_config_content = <<-JSON
 {
     "install_path": "c:\\\\ops\\\\consul\\\\bin",
     "config_path": "c:\\\\meta\\\\consul",
 }
-  JSON
-  it 'creates the service_consul.json meta file' do
-    expect(chef_run).to create_file("#{meta_directory}\\service_consul.json").with_content(consul_service_config_content)
-  end
-
-  it 'opens the TCP ports for consul in the firewall' do
-    expect(chef_run).to create_windows_firewall_rule('Consul_TCP').with(
-        values: [{
-            dir: 'in',
-            firewall_action: :allow,
-            protocol: 'TCP',
-            program: "#{consul_bin_directory}\\consul.exe",
-            profile: 'domain'
-        }])
-  end
-
-  it 'opens the UDP ports for consul in the firewall' do
-    expect(chef_run).to create_windows_firewall_rule('Consul_UDP').with(
-        values: [{
-            dir: 'in',
-            firewall_action: :allow,
-            protocol: 'UDP',
-            program: "#{consul_bin_directory}\\consul.exe",
-            profile: 'domain'
-        }])
+    JSON
+    it 'creates the service_consul.json meta file' do
+      expect(chef_run).to create_file("#{meta_directory}\\service_consul.json").with_content(consul_service_config_content)
+    end
   end
 end
