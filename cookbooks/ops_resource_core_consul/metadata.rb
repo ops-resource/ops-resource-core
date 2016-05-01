c0bbcae7b36e598a6cf964c5112479b653d905ab
Petrik van der Velde <petrikvandervelde@gmail.com>
2016-05-01 12:16:59 +1200

    Fixing the license on all the cookbooks in the metadata.rb file

--- cookbooks/ops_resource_core_consul/metadata.rb
+++ cookbooks/ops_resource_core_consul/metadata.rb
@@ -1,7 +1,7 @@
 name 'ops_resource_core_consul'
 maintainer '${CompanyName} (${CompanyUrl})'
 maintainer_email '${EmailDocumentation}'
-license 'All rights reserved'
+license 'Apache v2.0'
 description 'Configures a server with a consul service'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '${VersionSemantic}'
--- cookbooks/ops_resource_core_provisioning/metadata.rb
+++ cookbooks/ops_resource_core_provisioning/metadata.rb
@@ -1,7 +1,7 @@
 name 'ops_resource_core_provisioning'
 maintainer '${CompanyName} (${CompanyUrl})'
 maintainer_email '${EmailDocumentation}'
-license 'All rights reserved'
+license 'Apache v2.0'
 description 'Configures a server with the scripts and tools necessary to provide configuration and provisioning upon adding to an environment.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '${VersionSemantic}'
